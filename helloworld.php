796dbc0dd93ec6436fc8df0156f0071148057995
hexapetalous <hexapetalous@126.com>
2016-02-27 09:41:11 +0800

    Init!

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wan
+ * Date: 2/27/16
+ * Time: 9:18 AM
+ */
+?>
+<html>
+<head>
+    <title>hi</title>
+</head>
+<body>
+<?php
+echo 'hello, PHP!'
+?>
+</body>
+</html>
